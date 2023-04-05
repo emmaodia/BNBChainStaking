7e50debeb416a6a708716f054b0f3f46b681da6d
emmaodia <odia.emma@gmail.com>
2023-04-05 11:33:51 +0100

    UG Token Tests

--- test/ugtoken.js
+++ test/ugtoken.js
@@ -3,20 +3,53 @@
 const UGToken = artifacts.require("UGToken");
 
 contract("UGToken", (accounts) => {
-  it("matches name successfully", async () => {
-    let ugToken = await UGToken.new();
+  before(async () => {
+    this.ugToken = await UGToken.new();
+  });
 
-    const name = await ugToken.name();
+  it("matches name successfully", async () => {
+    const name = await this.ugToken.name();
 
     assert.equal(name, "UGToken");
   });
 
   it("can mint tokens to an address", async () => {
-    const ugtoken = await UGToken.deployed();
-
-    const check = await ugtoken.mint(accounts[0], 10000);
+    const check = await this.ugToken.mint(accounts[0], 10000);
     const amount = await check.logs[0].args.value.toNumber();
 
     assert.equal(amount, 10000, "10000 wasn't in the first account");
+  });
+  it("can verify that the owner is the zero address", async () => {
+    const owner = await this.ugToken.owner();
+    console.log(owner, accounts[0]);
+
+    assert.equal(owner, accounts[0], "Owner is not address at Zero index");
+  });
+  // it("can verify that the balanceOf owner is the totalSupply of minted tokens", async () => {
+  //   console.log(this.ugToken);
+  //   const balanceOf = await this.ugToken.balanceOf(accounts[0]);
+  //   console.log(balanceOf.toNumber());
+  //   assert.equal(balanceOf.toString(), "10000000", "Balance is not equal");
+  // });
+  it("can transfer tokens to another address", async () => {
+    await this.ugToken.transfer(accounts[2], 3000);
+    const A = await this.ugToken.balanceOf(accounts[2]);
+    console.log(A.toNumber());
+    assert.equal(A.toNumber(), 3000, "The Transferred amount is not 30000");
+  });
+  it("can approve another address to spend its tokens", async () => {
+    await this.ugToken.approve(accounts[0], 300, { from: accounts[2] });
+    await this.ugToken.transferFrom(accounts[2], accounts[1], 300);
+
+    const newA = await this.ugToken.balanceOf(accounts[2]);
+    console.log(newA.toNumber());
+    assert.equal(newA.toNumber(), 2700, "The Balance is not 2700");
+  });
+  it("can transfer from index2 account to any other account", async () => {
+    await this.ugToken.transfer(accounts[1], 300, { from: accounts[2] });
+    const B = await this.ugToken.balanceOf(accounts[1]);
+    console.log(B.toNumber());
+
+    assert.equal(B.toNumber(), 600, "Balance is not equal to 600");
   });
 });
