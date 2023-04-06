e11b5bf53a6cc37aad841dd5a2371e581ba692de
emmaodia <odia.emma@gmail.com>
2023-04-06 11:13:14 +0100

    Removed Commented Code code from BreadToken Contract. Set up a MockERC contract for running tests

--- contracts/bsc/BreadToken.sol
+++ contracts/bsc/BreadToken.sol
@@ -1,16 +1,5 @@
 // SPDX-License-Identifier: GPL-3
 
-// pragma solidity ^0.6.0;
-
-// import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
-
-// contract BreadToken is BEP20("Bread Token", "BREAD") {
-//     function mint(address _to, uint256 _amount) public onlyOwner {
-//         _to = msg.sender;
-//         _mint(_to, _amount);
-//     }
-// }
-
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
