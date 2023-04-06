b780081237370724313551e7a4163552824100ce
emmaodia <odia.emma@gmail.com>
2023-04-06 11:13:51 +0100

    migrations file update

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,9 +1,11 @@
 const BreadToken = artifacts.require("BreadToken");
+const MockERC20 = artifacts.require("MockERC20");
 const UGToken = artifacts.require("UGToken");
 const Staking = artifacts.require("Staking");
 
 module.exports = function (deployer) {
   deployer.deploy(BreadToken, "10000");
+  deployer.deploy(MockERC20);
   deployer.deploy(UGToken).then(function () {
     return deployer.deploy(
       Staking,
